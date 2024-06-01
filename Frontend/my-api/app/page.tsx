86ba306d489acc5629c2d8467916fb50e681e500
ouffad_y <ouffad_y@etna-alternance.net>
2024-06-01 12:18:31 +0000

    add_API-Djang

--- Frontend/my-api/app/page.tsx
+++ Frontend/my-api/app/page.tsx
@@ -1,200 +1,124 @@
-// // import Image from "next/image";
+// import React from 'react';
 
-// // export default function Home() {
-// //   return (
-// //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-// //       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-// //         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-// //           Get started by editing&nbsp;
-// //           <code className="font-mono font-bold">app/page.tsx</code>
-// //         </p>
-// //         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-// //           <a
-// //             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-// //             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// //             target="_blank"
-// //             rel="noopener noreferrer"
-// //           >
-// //             By{" "}
-// //             <Image
-// //               src="/vercel.svg"
-// //               alt="Vercel Logo"
-// //               className="dark:invert"
-// //               width={100}
-// //               height={24}
-// //               priority
-// //             />
-// //           </a>
-// //         </div>
-// //       </div>
+// export default function Page() {
+//   return (
+//     <div>
+//       <header className="bg-red-600">
+//         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+//           <div className="flex h-16 items-center justify-between">
+//             <div className="hidden md:block flex-grow"> 
+//               <nav aria-label="Global">
+//                 <ul className="flex items-center justify-center gap-6 text-sm">
+//                   <li>
+//                     <a className="text-gray-500 transition hover:text-gray-500/75" href="/products"> Products </a>
+//                   </li>
+//                 </ul>
+//               </nav>
+//             </div>
 
-// //       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-// //         <Image
-// //           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-// //           src="/next.svg"
-// //           alt="Next.js Logo"
-// //           width={180}
-// //           height={37}
-// //           priority
-// //         />
-// //       </div>
+//             <div className="flex items-center gap-4">
+//               <div className="sm:flex sm:gap-4">
+//                 <a
+//                   className="rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+//                   href="/login"
+//                 >
+//                   Login
+//                 </a>
 
-// //       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-// //         <a
-// //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           <h2 className="mb-3 text-2xl font-semibold">
-// //             Docs{" "}
-// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-// //               -&gt;
-// //             </span>
-// //           </h2>
-// //           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-// //             Find in-depth information about Next.js features and API.
-// //           </p>
-// //         </a>
+//                 <div className="hidden sm:flex">
+//                   <a
+//                     className="rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+//                     href="/register"
+//                   >
+//                     Register
+//                   </a>
+//                 </div>
+//               </div>
 
-// //         <a
-// //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           <h2 className="mb-3 text-2xl font-semibold">
-// //             Learn{" "}
-// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-// //               -&gt;
-// //             </span>
-// //           </h2>
-// //           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-// //             Learn about Next.js in an interactive course with&nbsp;quizzes!
-// //           </p>
-// //         </a>
-
-// //         <a
-// //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           <h2 className="mb-3 text-2xl font-semibold">
-// //             Templates{" "}
-// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-// //               -&gt;
-// //             </span>
-// //           </h2>
-// //           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-// //             Explore starter templates for Next.js.
-// //           </p>
-// //         </a>
-
-// //         <a
-// //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           <h2 className="mb-3 text-2xl font-semibold">
-// //             Deploy{" "}
-// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-// //               -&gt;
-// //             </span>
-// //           </h2>
-// //           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-// //             Instantly deploy your Next.js site to a shareable URL with Vercel.
-// //           </p>
-// //         </a>
-// //       </div>
-// //     </main>
+//               <div className="block md:hidden">
+//                 <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+//                   {/* <svg
+//                     xmlns="http://www.w3.org/2000/svg"
+//                     className="h-5 w-5"
+//                     fill="none"
+//                     viewBox="0 0 24 24"
+//                     stroke="currentColor"
+//                     strokeWidth="2"
+//                   >
+//                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+//                   </svg> */}
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </header>
+//     </div>
 //   );
 // }
+import React from 'react';
 
-import Image from "next/image";
-
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-[#800020] min-h-screen"> {/* Ajout de la classe bg-[#800020] pour la couleur bordo et min-h-screen pour la hauteur minimale */}
+      <header className="bg-red-300 border-b-4 border-red-800">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="hidden md:block flex-grow">
+              <nav aria-label="Global">
+                <ul className="flex items-center justify-center gap-6 text-sm">
+                  <li>
+                    <a className="text-blue-500 transition hover:text-gray-800/100" href="/products"> Products </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="rounded-md bg-blue-400 px-5 py-2.5 text-sm font-medium text-white shadow"
+                  href="/login"
+                >
+                  Login
+                </a>
+
+                <div className="hidden sm:flex">
+                  <a
+                    className="rounded-md bg-blue-400 px-5 py-2.5 text-sm font-medium text-white shadow"
+                    href="/register"
+                  >
+                    Register
+                  </a>
+                </div>
+              </div>
+
+              <div className="block md:hidden">
+                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  {/* <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg> */}
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className="absolute h-[300px] w-full -translate-x-1/2 rounded-full bg-gradient-radial from-white to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-blue-700 dark:opacity-10 sm:w-[480px]"></div>
-        <div className="absolute -z-20 h-[180px] w-full translate-x-1/3 bg-gradient-conic from-sky-200 via-blue-200 blur-2xl dark:from-sky-900 dark:via-[#0141ff] dark:opacity-40 sm:w-[240px]"></div>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {[
-          {
-            href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
-            title: "Docs",
-            description: "Find in-depth information about Next.js features and API."
-          },
-          {
-            href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
-            title: "Learn",
-            description: "Learn about Next.js in an interactive course with quizzes!"
-          },
-          {
-            href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
-            title: "Templates",
-            description: "Explore starter templates for Next.js."
-          },
-          {
-            href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
-            title: "Deploy",
-            description: "Instantly deploy your Next.js site to a shareable URL with Vercel."
-          }
-        ].map((link, index) => (
-          <a
-            key={index}
-            href={link.href}
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              {link.title}{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">{link.description}</p>
-          </a>
-        ))}
-      </div>
-    </main>
+      </header>
+    </div>
   );
 }
+
+
+
+
+
+
+
+
--- Frontend/my-api/app/products/page.tsx
+++ Frontend/my-api/app/products/page.tsx
@@ -1,47 +1,100 @@
 
 
 
+// import React from 'react';
+
+// export default function Products() {
+//   const products = [
+//     { id: 1, image: "/image/tableau1.jpeg", price: "200€" },
+//     { id: 2, image: "/image/tableau2.jpeg", price: "200€" },
+//     { id: 3, image: "/image/tableau3.jpeg", price: "200€" },
+//     { id: 4, image: "/image/tableau4.jpeg", price: "200€" },
+//     { id: 5, image: "/image/tableau5.jpeg", price: "200€" },
+//     { id: 6, image: "/image/tableau6.jpeg", price: "200€" },
+//     { id: 7, image: "/image/tableau7.jpeg", price: "200€" },
+//     { id: 8, image: "/image/tableau12.jpeg", price: "200€" },
+//     { id: 9, image: "/image/tableau10.jpeg", price: "200€" },
+//     { id: 10, image: "/image/tableau13.jpeg",price: "200€" },
+//     { id: 11, image: "/image/tableau14.jpeg", price: "200€" },
+//     { id: 12, image: "/image/tableau16.jpeg",  price: "200€" },
+//   ];
+
+//   return (
+//     <section>
+//       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+//         <header>
+//           <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Original Paintings For Sale</h2>
+//           <p className="mt-4 max-w-md text-gray-500">
+//           Welcome to my online store! I am a passionate artist creating unique and original paintings.
+//           </p>
+//         </header>
+
+//         <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+//           {products.map(product => (
+//             <li key={product.id}>
+//               <a href="#" className="group block overflow-hidden">
+//                 <img
+//                   src={product.image}
+//                   alt=""
+//                   className="h-32 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  
+//                 />
+//                 <div className="relative bg-white pt-3">
+//                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+//                     {product.name}
+//                   </h3>
+//                   <p className="mt-2">
+//                     <span className="sr-only">Regular Price</span>
+//                     <span className="tracking-wider text-gray-900">{product.price}</span>
+//                   </p>
+//                 </div>
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// }
+
 import React from 'react';
 
 export default function Products() {
   const products = [
-    { id: 1, image: "/image/tableau1.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 2, image: "/image/tableau2.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 3, image: "/image/tableau3.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 4, image: "/image/tableau4.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 5, image: "/image/tableau5.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 6, image: "/image/tableau6.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 7, image: "/image/tableau7.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 8, image: "/image/tableau12.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 9, image: "/image/tableau10.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 10, image: "/image/tableau13.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 11, image: "/image/tableau14.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
-    { id: 12, image: "/image/tableau16.jpeg", name: "Basic Tee", price: "£24.00 GBP" },
+    { id: 1, image: "/image/tableau1.jpeg", price: "200€" },
+    { id: 2, image: "/image/tableau2.jpeg", price: "200€" },
+    { id: 3, image: "/image/tableau3.jpeg", price: "200€" },
+    { id: 4, image: "/image/tableau4.jpeg", price: "200€" },
+    { id: 5, image: "/image/tableau5.jpeg", price: "200€" },
+    { id: 6, image: "/image/tableau6.jpeg", price: "200€" },
+    { id: 7, image: "/image/tableau7.jpeg", price: "200€" },
+    { id: 8, image: "/image/tableau12.jpeg", price: "200€" },
+    { id: 9, image: "/image/tableau10.jpeg", price: "200€" },
+    { id: 10, image: "/image/tableau13.jpeg", price: "200€" },
+    { id: 11, image: "/image/tableau14.jpeg", price: "200€" },
+    { id: 12, image: "/image/tableau16.jpeg", price: "200€" },
   ];
 
   return (
-    <section>
+    <section className="bg-[#800020] min-h-screen">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <header>
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Product Collection</h2>
-          <p className="mt-4 max-w-md text-gray-500">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure dicta incidunt est ipsam, officia dolor fugit natus?
+          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Original Paintings For Sale</h2>
+          <p className="mt-2 max-w-md ">
+            Welcome to my online store! I am a passionate artist creating unique and original paintings.
           </p>
         </header>
 
         <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
           {products.map(product => (
-            <li key={product.id}>
+            <li key={product.id} className="p-4 rounded-md bg-white">
               <a href="#" className="group block overflow-hidden">
                 <img
                   src={product.image}
                   alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  className="h-32 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
                 />
                 <div className="relative bg-white pt-3">
-                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                    {product.name}
-                  </h3>
                   <p className="mt-2">
                     <span className="sr-only">Regular Price</span>
                     <span className="tracking-wider text-gray-900">{product.price}</span>
@@ -55,3 +108,4 @@ export default function Products() {
     </section>
   );
 }
+
